5b75a48b201362ffd23a2a15dc1b0f56bf152d97
pengbd <961862555@qq.com>
2017-11-26 20:52:35 +0800

    一个能跑的遗传算法
    但是它还是又慢又蠢，跑10k次还是只能跑进误差66.7%

--- ga_pbd.py
+++ ga_pbd.py
@@ -2,89 +2,98 @@ import random
 import readin
 import read_ans
 
-def initial_dis(n, map):
-    global dis
-    for i in range(n):
-	    dis.append([])
-	    for j in range(n):
-	        dis[i].append(((map[i][0]-map[j][0])**2 + (map[i][1]-map[j][1])**2) ** 0.5)
-	    dis[i] = tuple(dis[i])
-    dis = tuple(dis)
-
-
-def dis_cal(path):
-    distance = 0
-    for i in range(n):
-	    distance += dis[path[i]][path[(i+1)%n]]
-    return distance
-
 class Life():
-    def __init__(self, order = None):
-        self.city_coordinate = order
+    def __init__(self, path = None):
+        self.gene_path = path
         self.score = -1
 
 class GA():
-    def __init__(self, iordinate, bordinate, crate, mrate, lcount, ccount):
-        self.init_ordinate = iordinate
-        self.best_ordinate = bordinate
+    def __init__(self, crate, mrate, lcount, tsp = r"data\eil101.tsp", opt = r"data\eil101.opt.tour"):
         self.cross_rate = crate
         self.mutation_rate = mrate
         self.life_count = lcount
-        self.city_count = ccount
+        self.map = []
+        self.std_path = []
+        self.city_count = 0
         self.lives = []
         self.best_life = None
+        self.best_dis = 0.0
+        self.relate_error = 100.0
         self.generation = 1
         self.cross_count = 0
         self.mutation_count = 0
         self.bounds = 0.0
+        self.dis = []
+        self.reinitialize(tsp, opt)
 
-        self.start_up()
-        initial_dis(self.city_count, self.init_ordinate)
-
-    def start_up(self):
+    def reinitialize(self, tsp, opt):
+        #readin
+        n, mp = readin.readin(tsp)
+        self.city_count = n
+        self.map = mp
+        sp = read_ans.read_ans(n, opt)
+        self.std_path = sp
+        #initial lives
         self.lives = []
         for i in range(self.life_count):
-            gene = self.init_ordinate
-            random.shuffle(gene)
-            life = Life(gene)
+            path = [x for x in range(self.city_count)]
+            random.shuffle(path)
+            life = Life(path)
             self.lives.append(life)
-    
+        #initial distance tuple
+        for i in range(n):
+            self.dis.append([])
+            for j in range(n):
+                self.dis[i].append(((mp[i][0]-mp[j][0])**2 + (mp[i][1]-mp[j][1])**2) ** 0.5)
+            self.dis[i] = tuple(self.dis[i])
+        self.dis = tuple(self.dis)
+        self.std_dis = self.dis_cal(self.std_path)
+
+    def dis_cal(self, path):
+        n = len(path)
+        distance = 0
+        for i in range(n):
+	        distance += self.dis[path[i]][path[(i+1)%n]]
+        return distance
+
     def judge(self):
         self.bounds = 0.0
         self.best_life = self.lives[0]
         for i in range(self.life_count):
-            self.lives[i].score = 1.0/dis_cal(self.lives[i].city_coordinate)
+            self.lives[i].score = 10000.0/self.dis_cal(self.lives[i].gene_path)
             self.bounds += self.lives[i].score
             if self.best_life.score < self.lives[i].score:
                 self.best_life = self.lives[i]
+        self.best_dis = self.dis_cal(self.best_life.gene_path)
+        self.relate_error = (self.best_dis - self.std_dis)/self.std_dis
 
     def cross(self, parent1, parent2):
         index1 = random.randint(0, self.city_count - 1)
         index2 = random.randint(index1, self.city_count - 1)
-        tempGene = parent2.city_coordinate[index1:index2]
-        newGene = []
-        tempCount = 0
-        for g in parent1.city_coordinate:
-            if tempCount == index1:
-                newGene.extend(tempGene)
-                tempCount += 1
-            if g not in tempGene:
-                newGene.append(g)
-                tempCount += 1
+        temp_path = parent2.gene_path[index1:index2]
+        new_path = []
+        p1len = 0
+        for g in parent1.gene_path:
+            if p1len == index1:
+                new_path.extend(temp_path)
+                p1len += 1
+            if g not in temp_path:
+                new_path.append(g)
+                p1len += 1
         self.cross_count += 1
-        return newGene
+        return new_path
 
-    def mutation(self, order):
+    def mutation(self, path):
         index1 = random.randint(0, self.city_count - 1)
         index2 = random.randint(0, self.city_count - 1)
-        newGene = order[:]
-        newGene[index1], newGene[index2] = newGene[index2], newGene[index1]
+        new_path = path[:]
+        new_path[index1], new_path[index2] = new_path[index2], new_path[index1]
         self.mutation_count += 1
-        return newGene
+        return new_path
 
     def getOne(self):
         r = random.uniform(0, self.bounds)
-        for i in range(self.city_count):
+        for i in range(self.life_count):
             r -= self.lives[i].score
             if r <= 0:
                 return self.lives[i]
@@ -95,15 +104,15 @@ class GA():
         rate1 = random.random()
         if rate1 < self.cross_rate:
             parent2 = self.getOne()
-            order = self.cross(parent1, parent2)
+            path = self.cross(parent1, parent2)
         else :
-            order = parent1.city_coordinate
+            path = parent1.gene_path
         
         rate2 = random.random()
         if rate2 < self.mutation_rate:
-            order = self.mutation(order)
+            path = self.mutation(path)
         
-        return Life(order)
+        return Life(path)
 
     def next_(self):
         self.judge()
@@ -113,34 +122,20 @@ class GA():
             newLives.append(self.newChild())
         self.lives = newLives
         self.generation += 1
-        self.judge()
-        return self.best_life.city_coordinate
+        show_path = [(self.map[i][0], self.map[i][1]) for i in self.best_life.gene_path]
+        show_path.append((self.map[self.best_life.gene_path[0]][0], self.map[self.best_life.gene_path[0]][1]))
+        return show_path, self.relate_error
 
-def read(tsp, opt):
-    n, map = readin.readin(tsp)
-    std_path = read_ans.read_ans(n, opt)
-    return n, map, std_path
+def initialize():
+    pass
 
 def next():
-    ga.next_()
-    bd = dis_cal(ga.best_life.coordinate)
-    print(ga.generation, " : ", bd)
-    #return ga.next_()
+    pass
 
 if __name__ == '__main__':
-    n, map, std_path = read(r"data\eil101.tsp", r"data\eil101.opt.tour")
-    ga = GA(map, std_path, 0.7, 0.02, 100, n)
-    for i in range(100):
+    ga = GA(0.7, 0.02, 200)
+    for i in range(10000):
         ga.next_()
-        bd = dis_cal(ga.best_life.coordinate)
-        print(ga.generation, " : ", bd)
-
-
-
-        
-
-    
-
-            
+        print(ga.generation, " distance: ", ga.best_dis, ", relative error: ", ga.relate_error*100, "%")
 
 
